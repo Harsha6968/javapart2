60f926ea4babaac573a4314f3442012dd26b41ef
harsha6968 <goneharshavardhan96962@gmail>
2022-12-06 12:45:43 +0530

    hi

--- one.js
+++ one.js
@@ -1,4 +1,28 @@
 function display(){
-    alert("hi hello")
-    imgsrc="./harsha4.jpg", alt="one.js", height="100px", width="200"
+    console.log("hi hello")
+document.getElementById('abc').style.backgroundColor='green'
+}
+function mul(){
+document.getElementById('xyz').style.backgroundColor='blue'
+}
+function Sub(){
+document.getElementById('pqr').style.backgroundColor='pink'
+}
+function add(){
+document.getElementById('lmn').style.backgroundColor='green'
+}
+function changecolor(){
+document.getElementById('cde').style.backgroundColor='violet'
+
+}
+function changeCase(){
+  let name=document.getElementById('abc').value
+  document.getElementById('abc').value = name.toUpperCase()
+}
+
+function changeCase() {
+    let name = document.getElementById('uname').value;
+    console.log(name)
+    //document.getElementById('uname').value = "gm"
+    document.getElementById('uname').value = name.toUpperCase()
 }
